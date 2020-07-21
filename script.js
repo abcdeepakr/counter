1d169e4a9622c5b8bc6cd1001813114e4dffc767
Deepak Rawat <33338977+DeepakR-28@users.noreply.github.com>
2020-07-21 15:30:43 +0530

    added a sequence

--- script.js
+++ script.js
@@ -1,40 +1,48 @@
 var plusOne = document.getElementById("plusOne"),
   count = 0;
+sequence = "";
 plusOne.onclick = function () {
   count += 1;
   document.getElementById("update").innerHTML = count;
+  sequence += "+1";
 };
 
 var plusFive = document.getElementById("plusFive");
 plusFive.onclick = function () {
   count += 5;
   document.getElementById("update").innerHTML = count;
+  sequence += "+5";
 };
 
 var plusTen = document.getElementById("plusTen");
 plusTen.onclick = function () {
   count += 10;
   document.getElementById("update").innerHTML = count;
+  sequence += "+10";
 };
 
 var minusOne = document.getElementById("minusOne");
 minusOne.onclick = function () {
   count -= 1;
   document.getElementById("update").innerHTML = count;
+  sequence += "-1";
 };
 
 var minusFive = document.getElementById("minusFive");
 minusFive.onclick = function () {
   count -= 5;
   document.getElementById("update").innerHTML = count;
+  sequence += "-5";
 };
 
 var minusTen = document.getElementById("minusTen");
 minusTen.onclick = function () {
   count -= 10;
   document.getElementById("update").innerHTML = count;
+  sequence += "-10";
 };
 
-function clear() {
-  document.getElementById("reset").reset();
+function seq() {
+  document.getElementById("finalSequence").innerHTML =
+    "Your Sequence is " + sequence;
 }
